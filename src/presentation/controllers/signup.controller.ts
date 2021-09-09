2da9c37c0979602058b69d57841d440f1a0ea2ae
IglanCardeal <cmtcardeal@outlook.com>
2021-09-09 10:49:31 -0300

    feat: ensure SignUpController return an error if no email was provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,9 +1,16 @@
 export class SignUpController {
-  public handle (httpRequest: unknown): any {
-    (httpRequest)
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+  public handle (httpRequest: { body: { name: string; email: string } }): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
