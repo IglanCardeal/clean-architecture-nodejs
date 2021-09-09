8143fd31ad06f2045fb6b6e49affd382fa0267a4
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-09 13:17:49 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws exception

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,30 +1,43 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor (private readonly emailValidator: EmailValidator) {}
 
-  public handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+  public handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return {
-      statusCode: 201,
-      body: {
-        message: 'success'
+      return {
+        statusCode: 201,
+        body: {
+          message: 'success'
+        }
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
   }
