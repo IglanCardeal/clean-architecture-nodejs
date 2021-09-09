1d083e5d6582e17ce47ac94b3d5a2d5b49de746e
IglanCardeal <cmtcardeal@outlook.com>
2021-09-09 09:47:17 -0300

    chore: adjust jest config to match only '*.test.ts' files and display 'unit-test' info

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  displayName: 'unit-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
