918194a989d990b6b08b91578aa3da6c59ff8bd2
IglanCardeal <cmtcardeal@outlook.com>
2021-09-10 18:20:20 -0300

    test: ensure SignUpController returns 200 if valid data was provided

--- src/presentation/controllers/signup/signup-controller.test.ts
+++ src/presentation/controllers/signup/signup-controller.test.ts
@@ -230,4 +230,25 @@ describe('SignUp Controller', () => {
       password: '123456'
     })
   })
+
+  it('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid name',
+        email: 'valid@email.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })
