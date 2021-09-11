22174e3e2e3eb806ccb137fb6e4396ed61c62a92
IglanCardeal <cmtcardeal@outlook.com>
2021-09-11 00:25:17 -0300

    test: ensure DbAddAccount returns account on success

--- src/data/usecases/add-account/db-add-account.test.ts
+++ src/data/usecases/add-account/db-add-account.test.ts
@@ -102,4 +102,20 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
     expect(promise).rejects.toThrow()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid name',
+      email: 'valid@email.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid name',
+      email: 'valid@email.com',
+      password: 'hashed password'
+    })
+  })
 })
