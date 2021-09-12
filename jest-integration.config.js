d86f5cdf5dc4a69f5b1a5a7e5cc94dc1dcc0c1fc
IglanCardeal <cmtcardeal@outlook.com>
2021-09-12 15:51:22 -0300

    chore: adjust jest config for integration tests

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,5 +1,6 @@
 // faz o match para teste de integração com a extensão *.spec.ts
 const rootConfig = require('./jest.config')
 rootConfig.testMatch = ['<rootDir>/src/**/*.spec.ts']
+rootConfig.displayName = 'integration-tests'
 
 module.exports = rootConfig
