9f6e53943965d8e79107f24f321b562ebd8eff20
IglanCardeal <cmtcardeal@outlook.com>
2021-09-18 18:46:44 -0300

    test: ensure requests parse body as json format

---
+++ src/main/middlewares/body-parser.spec.ts
@@ -0,0 +1,15 @@
+import supertest from 'supertest'
+
+import { app } from '../express/app'
+
+describe('Body Parser Middleware', () => {
+  it('Should parse body as json', async () => {
+    app.post('/test_parser', (req, res) => {
+      return res.send(req.body)
+    })
+    await supertest(app)
+      .post('/test_parser')
+      .send({ name: 'test' })
+      .expect({ name: 'test' })
+  })
+})
