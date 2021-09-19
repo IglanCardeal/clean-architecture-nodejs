e76d667614d5f82bd3f1997b85b3829bb2c13200
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-19 07:07:18 -0300

    feat: ensure content type returns as json as default

--- src/main/express/middlewares-setup.ts
+++ src/main/express/middlewares-setup.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
 import { cors } from '../middlewares/cors'
 
 export const middlewaresSetup = (app: Express) => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
---
+++ src/main/middlewares/content-type.spec.ts
@@ -0,0 +1,24 @@
+import supertest from 'supertest'
+
+import { app } from '../express/app'
+
+describe('Content Type Middleware', () => {
+  it('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      return res.send()
+    })
+    await supertest(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  it('Should return default content type as json', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      return res.send()
+    })
+    await supertest(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
