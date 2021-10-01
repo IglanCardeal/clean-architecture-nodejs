b535c7387be378d4b33451d895e06ad55f70c008
IglanCardeal <cmtcardeal@outlook.com>
2021-10-01 01:25:26 -0300

    refactor: make serverError function returns error stack

--- src/modules/account/usecases/signup/signup-controller.ts
+++ src/modules/account/usecases/signup/signup-controller.ts
@@ -49,7 +49,7 @@ export class SignUpController implements Controller {
 
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error as Error)
     }
   }
 }
