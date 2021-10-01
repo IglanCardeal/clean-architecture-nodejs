2c7371c02a98232d461489adeadbc645ecd79256
IglanCardeal <cmtcardeal@outlook.com>
2021-10-01 11:36:56 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/infra/decorators/log.test.ts
+++ src/infra/decorators/log.test.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { serverError } from '@src/shared/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@src/shared/ports'
+import { LogErrorRepository } from '@src/shared/ports/log-repository'
 import { LogControllerDecorator } from './log'
 
 class GenericControllerStub implements Controller {
@@ -11,11 +13,22 @@ class GenericControllerStub implements Controller {
   }
 }
 
+class LogErrorRepositoryStub implements LogErrorRepository {
+  async log (stack: string): Promise<void> {
+    return
+  }
+}
+
 const makeSut = () => {
   const genericControllerStub = new GenericControllerStub()
+  const logErrorRepositoryStub = new LogErrorRepositoryStub()
   return {
-    sut: new LogControllerDecorator(genericControllerStub),
-    genericControllerStub
+    sut: new LogControllerDecorator(
+      genericControllerStub,
+      logErrorRepositoryStub
+    ),
+    genericControllerStub,
+    logErrorRepositoryStub
   }
 }
 
@@ -40,5 +53,23 @@ describe('LogController Decorator', () => {
       statusCode: 200,
       body: {}
     })
+  })
+
+  it('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, genericControllerStub, logErrorRepositoryStub } = makeSut()
+    const fakeError = new Error()
+    fakeError.stack = 'Fake Error Stack'
+    const error = serverError(fakeError)
+    jest
+      .spyOn(genericControllerStub, 'handle')
+      .mockImplementationOnce(async () => {
+        return error
+      })
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const httpRequest = {
+      body: {}
+    }
+    await sut.handle(httpRequest)
+    expect(logSpy).toHaveBeenCalledWith('Fake Error Stack')
   })
 })
