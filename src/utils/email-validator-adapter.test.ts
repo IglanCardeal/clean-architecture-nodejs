9f82df4d26c08fd6833aaec3f2fb888188295d7e
IglanCardeal <cmtcardeal@outlook.com>
2022-07-17 19:35:46 -0300

    test: ensure EmailValidatorAdapter throw if validator throws

--- src/utils/email-validator-adapter.test.ts
+++ src/utils/email-validator-adapter.test.ts
@@ -24,6 +24,14 @@ describe('EmailValidator Adapter', () => {
     expect(isValid).toBe(true)
   })
 
+  it('Should throw if validator throws', () => {
+    jest.spyOn(validator, 'isEmail').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const sut = makeSut()
+    expect(() => sut.isValid('invalid_email.com')).toThrow()
+  })
+
   it('Should call validator with correct email', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
