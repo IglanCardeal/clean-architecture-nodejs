99efc93ca06fd9cf8d686fe560ffe9ccb6322aa0
IglanCardeal <cmtcardeal@outlook.com>
2022-07-17 17:43:25 -0300

    feat: use generics for data type on http helpers

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -11,7 +11,7 @@ export const serverError = (): HttpResponse => ({
   body: new ServerError()
 })
 
-export const ok = (data: any): HttpResponse => ({
+export const ok = <T>(data: T): HttpResponse => ({
   statusCode: 200,
   body: data
 })
