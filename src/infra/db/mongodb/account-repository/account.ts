6ec36239f52cc2e5ebab74319f8bb47d49d98469
IglanCardeal <cmtcardeal@outlook.com>
2022-07-20 22:33:36 -0300

    feat: ensure AccountMongoRepository returns an AccountModel on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '@src/data/protocols/add-account-repository'
+import { AccountModel } from '@src/domain/models/account'
+import { AddAccountModel } from '@src/domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne({ ...account })
+    return { ...account, id: insertedId.toString() }
+  }
+}
