3d4bac98534d7215e13c72627c02012fd2568750
IglanCardeal <cmtcardeal@outlook.com>
2022-07-20 22:32:46 -0300

    feat: create a get collection method on MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: {} as MongoClient,
@@ -9,5 +9,9 @@ export const MongoHelper = {
 
   async disconnect() {
     await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
