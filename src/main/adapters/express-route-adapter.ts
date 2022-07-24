65ae8bb26560b8bc5208ceaf3e8c38f1c7b0141e
IglanCardeal <cmtcardeal@outlook.com>
2022-07-24 17:12:57 -0300

    feat: ensure express route adapter returns generic internal server error message

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,6 +9,11 @@ export const routeAdapter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = { body: req.body }
     const httpResponse: HttpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      return res.status(httpResponse.statusCode).json({
+        message: 'Internal Server Error'
+      })
+    }
     return res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
