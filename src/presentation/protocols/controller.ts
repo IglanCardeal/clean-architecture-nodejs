3ddfed2ff5f372c63dd46530607e14a2e4ca9a8c
IglanCardeal <cmtcardeal@outlook.com>
2022-07-24 17:58:23 -0300

    refactor: controller handle format

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,5 @@
-import { HttpRequest, HttpResponse } from "./http"
+import { HttpRequest, HttpResponse } from './http'
 
 export interface Controller {
-  handle: (httRequest: HttpRequest) => Promise<HttpResponse>
+  handle(httRequest: HttpRequest): Promise<HttpResponse>
 }
