62f5ac238412c552c08d227212c4fa28c638e155
IglanCardeal <cmtcardeal@outlook.com>
2022-07-30 00:47:36 -0300

    test: ensure ValidationComposite returns falsy if all validation succeeds

--- src/presentation/helpers/validation/validation-composite.test.ts
+++ src/presentation/helpers/validation/validation-composite.test.ts
@@ -5,7 +5,7 @@ import { ValidationComposite } from './validation-composite'
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate(_input: any): void | Error {
-      return new MissingParamError('field')
+      return undefined
     }
   }
   return new ValidationStub()
@@ -17,7 +17,16 @@ const makeSut = () => new ValidationComposite([validationStub])
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
     const sut = makeSut()
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  it('Should not return an error if all validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).toBeFalsy()
   })
 })
