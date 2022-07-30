5cf8f40d10e61d68d6f48c9062c5127b28fdf1fa
IglanCardeal <cmtcardeal@outlook.com>
2022-07-30 01:00:08 -0300

    test: ensure ValidationComposite returns the first error if more than one fails

--- src/presentation/helpers/validation/validation-composite.test.ts
+++ src/presentation/helpers/validation/validation-composite.test.ts
@@ -11,20 +11,31 @@ const makeValidation = (): Validation => {
   return new ValidationStub()
 }
 
-const validationStub = makeValidation()
-const makeSut = () => new ValidationComposite([validationStub])
+const validationsStub = [makeValidation(), makeValidation()]
+const makeSut = () => new ValidationComposite(validationsStub)
 
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
     const sut = makeSut()
     jest
-      .spyOn(validationStub, 'validate')
+      .spyOn(validationsStub[0], 'validate')
       .mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('field'))
   })
 
+  it('Should return the first error if more than one validation fails', () => {
+    const sut = makeSut()
+    jest.spyOn(validationsStub[0], 'validate').mockReturnValueOnce(new Error())
+    jest
+      .spyOn(validationsStub[1], 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({})
+    expect(error).toEqual(new Error())
+  })
+
   it('Should not return an error if all validation succeeds', () => {
+    jest.resetAllMocks()
     const sut = makeSut()
     const error = sut.validate({})
     expect(error).toBeFalsy()
