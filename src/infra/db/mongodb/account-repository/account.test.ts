bea62e57b03c9987af104ff18adb8045066cd3ef
IglanCardeal <cmtcardeal@outlook.com>
2022-11-30 22:02:32 -0300

    test: ensure AccountMongoRepository load account by email correctly

--- src/infra/db/mongodb/account-repository/account.test.ts
+++ src/infra/db/mongodb/account-repository/account.test.ts
@@ -4,6 +4,15 @@ import { AccountMongoRepository } from './account'
 const makeSut = () => new AccountMongoRepository()
 
 describe('Account MongoDB Repository', () => {
+  const insertFakeAccount = async ({ name, email, password }: any) => {
+    const accounts = await MongoHelper.getCollection('accounts')
+    await accounts.insertOne({
+      name,
+      email,
+      password
+    })
+  }
+
   beforeAll(async () => {
     await MongoHelper.connect()
   })
@@ -17,7 +26,7 @@ describe('Account MongoDB Repository', () => {
     await accounts.deleteMany({})
   })
 
-  it('Should return an account on success', async () => {
+  it('Should return an account on add success', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'valid_name',
@@ -33,5 +42,29 @@ describe('Account MongoDB Repository', () => {
         password: 'hashed_password'
       })
     )
+  })
+
+  it('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await insertFakeAccount({
+      name: 'valid_name2',
+      email: 'valid_email2@mail.com',
+      password: 'hashed_password2'
+    })
+    const account = await sut.loadByEmail('valid_email2@mail.com')
+    expect(account).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'valid_name2',
+        email: 'valid_email2@mail.com',
+        password: 'hashed_password2'
+      })
+    )
+  })
+
+  it('Should return undefined if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('valid_email2@mail.com')
+    expect(account).toBeUndefined()
   })
 })
