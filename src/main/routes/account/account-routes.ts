e1fb87526c5ee1672faa734c4061a356f6d88dc8
IglanCardeal <cmtcardeal@outlook.com>
2022-12-08 22:53:08 -0300

    refactor: remove unnecessary constants

--- src/main/routes/account/account-routes.ts
+++ src/main/routes/account/account-routes.ts
@@ -5,13 +5,16 @@ import {
   makeSignUpController
 } from '@src/main/factories/account'
 
-const signUpRouteAdapted = routeAdapter(makeSignUpController())
-const loginRouteAdapted = routeAdapter(makeLoginController())
-
 // `/api/ACCOUNT_ROUTE_PREFIX/...`
 export const ACCOUNT_ROUTE_PREFIX = 'account'
 
 export const accountRoutesSetup = (router: Router) => {
-  router.post(`/${ACCOUNT_ROUTE_PREFIX}/signup`, signUpRouteAdapted)
-  router.post(`/${ACCOUNT_ROUTE_PREFIX}/login`, loginRouteAdapted)
+  router.post(
+    `/${ACCOUNT_ROUTE_PREFIX}/signup`,
+    routeAdapter(makeSignUpController())
+  )
+  router.post(
+    `/${ACCOUNT_ROUTE_PREFIX}/login`,
+    routeAdapter(makeLoginController())
+  )
 }
