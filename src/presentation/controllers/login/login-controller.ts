5fb33e6685a0691d5238c07638812cd406efce5d
IglanCardeal <cmtcardeal@outlook.com>
2022-12-29 18:26:45 -0300

    refactor: create a handle failure method for login controller

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,4 +1,10 @@
-import { DbAuthenticationUseCaseResult } from '@src/data/usecases/authentication/db-authentication-usecase-result'
+import {
+  DbAuthenticationUseCaseResult,
+  HasherComparerError,
+  LoadAccountByEmailRepositoryError,
+  TokenGeneratorError,
+  UpdateAccessTokenRepositoryError
+} from '@src/data/usecases/authentication/db-authentication-usecase-result'
 import { InvalidCredentialsError } from '@src/domain/errors'
 import {
   badRequest,
@@ -21,33 +27,49 @@ export class LoginController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const error = this.validation.validate(httpRequest.body)
+      const validationError = this.validation.validate(httpRequest.body)
 
-      if (error) {
-        return badRequest(error)
+      if (validationError) {
+        return badRequest(validationError)
       }
 
       const { email, password } = httpRequest.body
-      const result = await this.authenticationUseCase.auth({
-        email,
-        password
-      })
-
-      if (result.isFailure()) {
-        const { error } = result
-        switch (error.constructor) {
-          case InvalidCredentialsError:
-            return unauthorized(error)
-          default:
-            return serverError(error)
+      const authenticationUseCaseResult = await this.authenticationUseCase.auth(
+        {
+          email,
+          password
         }
+      )
+
+      if (authenticationUseCaseResult.isFailure()) {
+        return this.handleFailure(authenticationUseCaseResult.error)
       }
 
-      const { token } = result.data
+      const { token } = authenticationUseCaseResult.data
 
       return ok({ accessToken: token })
     } catch (error: any) {
       return serverError(error)
+    }
+  }
+
+  private handleFailure(
+    error:
+      | InvalidCredentialsError
+      | LoadAccountByEmailRepositoryError
+      | UpdateAccessTokenRepositoryError
+      | HasherComparerError
+      | TokenGeneratorError
+  ): HttpResponse {
+    switch (error.constructor) {
+      case InvalidCredentialsError:
+        return unauthorized(error)
+      case LoadAccountByEmailRepositoryError:
+      case UpdateAccessTokenRepositoryError:
+      case HasherComparerError:
+      case TokenGeneratorError:
+      default:
+        return serverError(error)
     }
   }
 }
