1e49d84984d442204765f7e3954c1352333f4f07
IglanCardeal <cmtcardeal@outlook.com>
2023-01-22 20:55:50 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add/add-survey-controller.ts
+++ src/presentation/controllers/survey/add/add-survey-controller.ts
@@ -1,9 +1,17 @@
 import { badRequest } from '@src/presentation/helpers/http'
 import { Validation } from './add-survey-protocols'
-import { Controller, HttpRequest, HttpResponse } from './add-survey-protocols'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  AddSurveyUseCase
+} from './add-survey-protocols'
 
 export class AddSurveyController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurveyUseCase: AddSurveyUseCase<any>
+  ) {}
 
   async handle(httRequest: HttpRequest): Promise<HttpResponse> {
     const validationError = this.validation.validate(httRequest.body)
@@ -11,6 +19,8 @@ export class AddSurveyController implements Controller {
     if (validationError) {
       return badRequest(validationError)
     }
+
+    await this.addSurveyUseCase.add(httRequest.body)
 
     return {} as any
   }
--- src/presentation/controllers/survey/add/add-survey-protocols.ts
+++ src/presentation/controllers/survey/add/add-survey-protocols.ts
@@ -1,1 +1,2 @@
 export * from '@src/presentation/protocols'
+export * from '@src/domain/usecases/add-survey'
