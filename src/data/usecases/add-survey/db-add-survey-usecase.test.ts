d36765f8a2d00c1aeedb6bee870c8a0c45799e99
IglanCardeal <cmtcardeal@outlook.com>
2023-01-25 22:51:16 -0300

    test: ensure DbAddSurveyUseCase returns AddSurveyRepositoryError if AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey-usecase.test.ts
+++ src/data/usecases/add-survey/db-add-survey-usecase.test.ts
@@ -1,5 +1,6 @@
 import { AddSurveyModel } from '@src/domain/usecases/add-survey'
 import { AddSurveyRepository } from './db-add-survey-usecase-protocols'
+import { AddSurveyRepositoryError } from './db-add-survey-usecase-result'
 import { DbAddSurveyUseCase } from './db-add-survey-usecase.'
 
 class AddSurveyRepositoryStub implements AddSurveyRepository {
@@ -27,11 +28,21 @@ const makeFakeSurveyData = () => ({
 })
 
 describe('DbAddSurveyUseCase', () => {
+  const surveyData = makeFakeSurveyData()
+  const { addSurveyRepositoryStub, sut } = makeSut()
+
   it('should call AddSurveyRepository with correct values', async () => {
-    const { addSurveyRepositoryStub, sut } = makeSut()
     const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
-    const surveyData = makeFakeSurveyData()
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
+  })
+
+  it('should return a AddSurveyRepositoryError if AddSurveyRepository throws', async () => {
+    jest
+      .spyOn(addSurveyRepositoryStub, 'add')
+      .mockRejectedValueOnce(new Error())
+    const result = await sut.add(surveyData)
+    const error = result.isFailure() && result.error
+    expect(error).toEqual(new AddSurveyRepositoryError())
   })
 })
