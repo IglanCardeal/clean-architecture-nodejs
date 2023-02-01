576f2cd901501ecd2bcedf75716507d1f3af8e20
IglanCardeal <cmtcardeal@outlook.com>
2023-02-01 14:28:14 -0300

    feat: ensure AuthMiddleware returns 500 if LoadAccountByTokenUseCase throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -5,7 +5,7 @@ import {
   LoadAccountByTokenUseCase,
   AccountModel
 } from './auth-middleware-protocols'
-import { forbidden, ok } from '@src/presentation/helpers/http'
+import { forbidden, ok, serverError } from '@src/presentation/helpers/http'
 import { AccessDeniedError } from '@src/presentation/errors'
 
 export class AuthMiddleware implements Middleware {
@@ -16,21 +16,25 @@ export class AuthMiddleware implements Middleware {
   ) {}
 
   async handle(httRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httRequest.headers?.['x-access-token']
+    try {
+      const accessToken = httRequest.headers?.['x-access-token']
 
-    if (!accessToken) {
-      return forbidden(new AccessDeniedError())
+      if (!accessToken) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      const loadAccountByTokenUseCaseResult =
+        await this.loadAccountByTokenUseCase.load({ accessToken })
+
+      if (loadAccountByTokenUseCaseResult instanceof AccessDeniedError) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      return ok({
+        accountId: loadAccountByTokenUseCaseResult.id
+      })
+    } catch (error: any) {
+      return serverError(error)
     }
-
-    const loadAccountByTokenUseCaseResult =
-      await this.loadAccountByTokenUseCase.load({ accessToken })
-
-    if (loadAccountByTokenUseCaseResult instanceof AccessDeniedError) {
-      return forbidden(new AccessDeniedError())
-    }
-
-    return ok({
-      accountId: loadAccountByTokenUseCaseResult.id
-    })
   }
 }
