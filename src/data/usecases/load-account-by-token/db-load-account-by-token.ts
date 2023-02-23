96ee6a5bb983bfc67fdd1144ff296d50f5793058
IglanCardeal <cmtcardeal@outlook.com>
2023-02-22 22:18:58 -0300

    refactor: move logics to private methods

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,6 +1,7 @@
 import { InvalidAccountTokenOrRoleError } from '@src/domain/errors'
-import { failure, success } from '@src/shared'
+import { Either, failure, success } from '@src/shared'
 import {
+  AccountModel,
   Decrypter,
   LoadAccountByTokenRepository,
   LoadAccountByTokenUseCase,
@@ -20,32 +21,59 @@ export class DbLoadAccountByTokenUsecase
     private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
   ) {}
 
-  async load(
-    props: LoadAccountByTokenUseCaseProps
-  ): Promise<DbLoadAccountByTokenUsecaseResult> {
-    let accountId: string
+  async load({
+    accessToken,
+    role
+  }: LoadAccountByTokenUseCaseProps): Promise<DbLoadAccountByTokenUsecaseResult> {
+    const decryptResult = await this.decryptAccessTokenAndReturnAccountId(
+      accessToken
+    )
 
-    try {
-      accountId = await this.decrypter.decrypt(props.accessToken)
-    } catch (error: any) {
-      return failure(new DecrypterError(error.stack))
+    if (decryptResult.isFailure()) {
+      return failure(decryptResult.error)
     }
 
-    let account
+    const accountId = decryptResult.data
+    const findAccountResult = await this.findAccountById(accountId, role)
 
-    try {
-      account = await this.loadAccountByTokenRepository.loadByToken(
-        accountId,
-        props.role
-      )
-    } catch (error: any) {
-      return failure(new LoadAccountByTokenRepositoryError(error.stack))
+    if (findAccountResult.isFailure()) {
+      return failure(findAccountResult.error)
     }
+
+    const account = findAccountResult.data
 
     if (!account) {
       return failure(new InvalidAccountTokenOrRoleError())
     }
 
     return success(account)
+  }
+
+  private async decryptAccessTokenAndReturnAccountId(
+    accessToken: string
+  ): Promise<Either<string, DecrypterError>> {
+    try {
+      const accountId = await this.decrypter.decrypt(accessToken)
+      return success(accountId)
+    } catch (error: any) {
+      return failure(new DecrypterError(error.stack))
+    }
+  }
+
+  private async findAccountById(
+    accountId: string,
+    role?: string
+  ): Promise<
+    Either<AccountModel | undefined, LoadAccountByTokenRepositoryError>
+  > {
+    try {
+      const account = await this.loadAccountByTokenRepository.loadByToken(
+        accountId,
+        role
+      )
+      return success(account)
+    } catch (error: any) {
+      return failure(new LoadAccountByTokenRepositoryError(error.stack))
+    }
   }
 }
