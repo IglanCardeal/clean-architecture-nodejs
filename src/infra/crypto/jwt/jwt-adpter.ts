06eb89c0fd3d8e89245f9525719d4958435c117a
IglanCardeal <cmtcardeal@outlook.com>
2023-02-23 09:11:39 -0300

    feat: ensure TokenGeneratorAdapter calls verify with correct values

--- src/infra/crypto/jwt/jwt-adpter.ts
+++ src/infra/crypto/jwt/jwt-adpter.ts
@@ -1,11 +1,16 @@
 import jwt from 'jsonwebtoken'
 
-import { TokenGenerator } from '@src/data/protocols/crypto'
+import { Decrypter, TokenGenerator } from '@src/data/protocols/crypto'
 
-export class TokenGeneratorAdapter implements TokenGenerator {
+export class TokenGeneratorAdapter implements TokenGenerator, Decrypter {
   constructor(private readonly jwtSecret: string) {}
 
   async generate(accountId: string): Promise<string> {
     return jwt.sign({ accountId }, this.jwtSecret)
+  }
+
+  async decrypt(token: string): Promise<string> {
+    jwt.verify(token, this.jwtSecret)
+    return ''
   }
 }
