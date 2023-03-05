8a9054a45b73e09a44b74f33467ea06e20cf436f
IglanCardeal <cmtcardeal@outlook.com>
2023-03-04 21:17:13 -0300

    test: ensure add survey route returns 204 when a valid access token is provided

--- src/main/routes/survey/survey-routes.spec.ts
+++ src/main/routes/survey/survey-routes.spec.ts
@@ -2,10 +2,39 @@ import { MongoHelper } from '@src/infra/db/mongodb/helpers/mongo-helper'
 import { app } from '@src/main/config/app'
 import request from 'supertest'
 import { SURVEYS_ROUTE_PREFIX } from './survey-routes'
+import { sign } from 'jsonwebtoken'
+import { Collection, Document } from 'mongodb'
+import { ENV } from '@src/main/config/env'
+
+const getValidAccessToken = async (accountCollection: Collection<Document>) => {
+  const res = await accountCollection.insertOne({
+    name: 'Foo',
+    email: 'foo@mail.com',
+    password: '123foo',
+    passwordConfirmation: '123foo',
+    role: 'admin'
+  })
+  const id = res.insertedId
+  const validToken = sign({ accountId: id }, ENV.jwtSecret)
+  await accountCollection.updateOne(
+    {
+      _id: id
+    },
+    {
+      $set: {
+        accessToken: validToken
+      }
+    }
+  )
+  return validToken
+}
 
 describe('Surveys Routes', () => {
+  let accountCollection: Collection<Document>
+
   beforeAll(async () => {
     await MongoHelper.connect()
+    accountCollection = await MongoHelper.getCollection('accounts')
   })
 
   afterAll(async () => {
@@ -15,6 +44,7 @@ describe('Surveys Routes', () => {
   beforeEach(async () => {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe(`POST /api/${SURVEYS_ROUTE_PREFIX}`, () => {
@@ -31,6 +61,23 @@ describe('Surveys Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    it('Should return status code 204 on add survey with valid access token', async () => {
+      const validToken = await getValidAccessToken(accountCollection)
+      await request(app)
+        .post(`/api/${SURVEYS_ROUTE_PREFIX}`)
+        .set('x-access-token', validToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
