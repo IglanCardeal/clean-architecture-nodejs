518e6ccca92ff084e1c879013645888101f49f5c
IglanCardeal <cmtcardeal@outlook.com>
2023-07-22 18:00:11 -0300

    fix: import of makeLoadSurveysController

--- src/main/factories/controllers/account/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/account/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from '@src/presentation/controllers/account/signup/signup-controller'
 import { makeSignUpValidations } from './signup-validations-factory'
-import { makeDbAddAccountUseCase } from '@src/main/factories/usecases/authentication/db-authentication-usecase-factory'
 import { makeDbAuthenticationUseCase } from '@src/main/factories/usecases/account/add-account/db-add-account-usescase-factory'
 import { makeLogControllerDecorator } from '@src/main/factories/controllers/decorators/log-controller-decorator-factory'
+import { makeDbAddAccountUseCase } from '@src/main/factories/usecases/account/authentication/db-authentication-usecase-factory'
 
 export const makeSignUpController = () => {
   const signUpController = new SignUpController(
