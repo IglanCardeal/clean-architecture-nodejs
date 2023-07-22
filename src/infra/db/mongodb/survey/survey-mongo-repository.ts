e286910c082f5d3263a6fa9efc239fe22b05583b
IglanCardeal <cmtcardeal@outlook.com>
2023-07-22 17:26:01 -0300

    feat: ensure SurveyMongoRepository return a list of all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,20 @@
-import { AddSurveyRepository } from '@src/data/protocols/db'
+import {
+  AddSurveyRepository,
+  ListSurveysRepository
+} from '@src/data/protocols/db'
 import { SurveyModel } from '@src/domain/models/survey'
 import { MongoHelper } from '@src/infra/db/mongodb/helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, ListSurveysRepository
+{
   async add(survey: SurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(survey)
+  }
+
+  async getList(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    return (await surveyCollection.find().toArray()) as unknown as SurveyModel[]
   }
 }
