d544604cb5e944511c7833f2486534600a82b410
IglanCardeal <cmtcardeal@outlook.com>
2023-07-23 11:08:38 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

--- src/main/factories/usecases/survey/load- surveys/db-load-surveys-usecase-factory.ts
+++ src/main/factories/usecases/survey/load- surveys/db-load-surveys-usecase-factory.ts
@@ -1,7 +1,8 @@
 import { DbLoadSurveysUseCase } from '@src/data/usecases/survey/load-surveys/db-load-surveys-usecase'
+import { LoadSurveyUseCase } from '@src/domain/usecases/survey'
 import { SurveyMongoRepository } from '@src/infra/db/mongodb/survey/survey-mongo-repository'
 
-export const makeDbLoadSurveysUseCase = (): DbLoadSurveysUseCase => {
+export const makeDbLoadSurveysUseCase = (): LoadSurveyUseCase => {
   const listSurveysRepository = new SurveyMongoRepository()
   return new DbLoadSurveysUseCase(listSurveysRepository)
 }
--- src/main/routes/survey/survey-routes.ts
+++ src/main/routes/survey/survey-routes.ts
@@ -3,15 +3,26 @@ import { routeAdapter } from '@src/main/adapters/express/express-route-adapter'
 import { makeAddSurveyController } from '@src/main/factories/controllers/survey'
 import { middlewareAdapter } from '@src/main/adapters/express/express-middleware-adapter'
 import { makeAuthMiddleware } from '@src/main/factories/controllers/middlewares/auth/auth-middleware-factory'
+import { makeLoadSurveysController } from '@src/main/factories/controllers/survey/load/load-surveys-controller-factory'
 
 // `/api/SURVEYS_ROUTE_PREFIX/...`
 export const SURVEYS_ROUTE_PREFIX = 'surveys'
 
 export const surveysRoutesSetup = (router: Router) => {
   const adminAuth = middlewareAdapter(makeAuthMiddleware('admin'))
+  const userAuth = middlewareAdapter(makeAuthMiddleware())
+
+  // create a survey
   router.post(
     `/${SURVEYS_ROUTE_PREFIX}`,
     adminAuth,
     routeAdapter(makeAddSurveyController())
+  )
+
+  // list all surveys
+  router.get(
+    `/${SURVEYS_ROUTE_PREFIX}`,
+    userAuth,
+    routeAdapter(makeLoadSurveysController())
   )
 }
