bb6370bdcac03f53a95880d50b76e153d54cbc31
IglanCardeal <cmtcardeal@outlook.com>
2023-07-26 20:37:52 -0300

    test: ensure DbSaveSurveyResultUseCase calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/survey/save-survey-result/db-save-survey-result-usecase.test.ts
@@ -0,0 +1,39 @@
+import { DbSaveSurveyResultUseCase } from './db-save-survey-result-usecase'
+import {
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel
+} from './db-save-survey-result-usecase-protocols'
+
+const anyDate = new Date()
+const makeFakeSaveSurveyResultModel = () => ({
+  surveyId: 'any_id',
+  accountId: 'any_id',
+  answer: 'any_answer',
+  date: anyDate
+})
+
+class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+  async save(_survey: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    return { ...makeFakeSaveSurveyResultModel(), id: 'any_id' }
+  }
+}
+
+const makeSut = () => {
+  const saveSurveyResultRepositoryStub = new SaveSurveyResultRepositoryStub()
+  return {
+    sut: new DbSaveSurveyResultUseCase(saveSurveyResultRepositoryStub),
+    saveSurveyResultRepositoryStub
+  }
+}
+
+describe('DbSaveSurveyResultUseCase', () => {
+  it('Should call SaveSurveyResultRepository with correct survey data', async () => {
+    const { saveSurveyResultRepositoryStub, sut } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+
+    await sut.save(makeFakeSaveSurveyResultModel())
+
+    expect(saveSpy).toHaveBeenCalledWith(makeFakeSaveSurveyResultModel())
+  })
+})
