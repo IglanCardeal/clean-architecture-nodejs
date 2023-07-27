74834a03819789e40c0279132ebca19ca83d3c13
IglanCardeal <cmtcardeal@outlook.com>
2023-07-26 21:11:01 -0300

    feat: ensure SurveyMongoRepository load a survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,12 +1,17 @@
 import {
   AddSurveyRepository,
-  ListSurveysRepository
+  ListSurveysRepository,
+  LoadSurveyByIdRepository
 } from '@src/data/protocols/db'
 import { SurveyModel } from '@src/domain/models/survey'
 import { MongoHelper } from '@src/infra/db/mongodb/helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository
-  implements AddSurveyRepository, ListSurveysRepository
+  implements
+    AddSurveyRepository,
+    ListSurveysRepository,
+    LoadSurveyByIdRepository
 {
   async add(survey: SurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
@@ -16,5 +21,12 @@ export class SurveyMongoRepository
   async getList(): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     return (await surveyCollection.find().toArray()) as unknown as SurveyModel[]
+  }
+
+  async load(surveyId: string): Promise<SurveyModel | null> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const data = await surveyCollection.findOne({ _id: new ObjectId(surveyId) })
+    if (!data) return null
+    return data as unknown as SurveyModel
   }
 }
