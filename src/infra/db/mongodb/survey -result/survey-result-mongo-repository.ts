634a80f9226c213da6e484f702a6a5f05d2978ad
IglanCardeal <cmtcardeal@outlook.com>
2023-07-30 16:02:23 -0300

    feat: ensure SurveyResultMongoRepository creates a new survey result

--- src/infra/db/mongodb/survey -result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey -result/survey-result-mongo-repository.ts
@@ -6,10 +6,22 @@ import { MongoHelper } from '@src/infra/db/mongodb/helpers/mongo-helper'
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save(survey: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyCollection = await MongoHelper.getCollection('surveyResults')
-    const inserted = await surveyCollection.insertOne(survey)
+    const inserted = await surveyCollection.findOneAndUpdate(
+      {
+        surveyId: survey.surveyId,
+        accountId: survey.accountId
+      },
+      {
+        $set: {
+          answer: survey.answer,
+          date: survey.date
+        }
+      },
+      { upsert: true, returnDocument: 'after' }
+    )
     return {
       ...survey,
-      id: inserted.insertedId.toString()
+      id: inserted.value?._id.toString() as string
     }
   }
 }
