36611d82a34dcc441647ceaed6752c82fb5430dc
IglanCardeal <cmtcardeal@outlook.com>
2024-03-29 14:58:21 -0300

    refactor: improve imports from protocols file

--- src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.protocols.ts
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.protocols.ts
@@ -1,1 +1,3 @@
 export * from '@src/data/protocols/db'
+export * from '@src/domain/usecases/survey/load-survey-result'
+export * from '@src/domain/models/survey-result'
--- src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.ts
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.ts
@@ -1,6 +1,8 @@
-import { LoadSurveyResultUseCase } from '@src/domain/usecases/survey/load-survey-result'
-import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-usecase-protocols'
-import { LoadSurveyResultRepository } from './db-load-survey-result-usecase.protocols'
+import {
+  LoadSurveyResultRepository,
+  SurveyResultModel,
+  LoadSurveyResultUseCase
+} from './db-load-survey-result-usecase.protocols'
 
 export class DbLoadSurveyResultUseCase implements LoadSurveyResultUseCase {
   constructor(
