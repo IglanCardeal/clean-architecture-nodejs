e11ce0fd626a35778bbea0f0f3faac73b7223ea0
IglanCardeal <cmtcardeal@outlook.com>
2024-03-30 12:09:12 -0300

    test: ensure SurveyResultMongoRepository loads survey result correctly

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.test.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.test.ts
@@ -29,7 +29,7 @@ describe('SurveyResultMongoRepository', () => {
     await surveyResultsCollection.deleteMany({})
   })
 
-  describe('save', () => {
+  describe('save()', () => {
     it('Should save a survey result if its a new one', async () => {
       const account = await makeAccount(accountCollection)
       const survey = await makeSurvey(surveyCollection)
@@ -71,6 +71,52 @@ describe('SurveyResultMongoRepository', () => {
       expect(updated.answers[0].percent).toBe(50)
       expect(updated.answers[1].count).toBe(1)
       expect(updated.answers[1].percent).toBe(50)
+    })
+  })
+
+  describe('loadBySurveyId()', () => {
+    it('Should update the survey result', async () => {
+      const account = await makeAccount(accountCollection)
+      const account2 = await makeAccount(accountCollection)
+      const account3 = await makeAccount(accountCollection)
+      const account4 = await makeAccount(accountCollection)
+      const survey = await makeSurvey(surveyCollection)
+      const saveSurveyResultParams: SaveSurveyResultParams = {
+        answer: survey.answers[0].answer,
+        accountId: account.id,
+        surveyId: survey.id as string,
+        date: new Date()
+      }
+      const saveSurveyResultParams2: SaveSurveyResultParams = {
+        ...saveSurveyResultParams,
+        answer: survey.answers[1].answer,
+        accountId: account2.id
+      }
+      const saveSurveyResultParams3: SaveSurveyResultParams = {
+        ...saveSurveyResultParams,
+        answer: survey.answers[1].answer,
+        accountId: account3.id
+      }
+      const saveSurveyResultParams4: SaveSurveyResultParams = {
+        ...saveSurveyResultParams,
+        answer: survey.answers[1].answer,
+        accountId: account4.id
+      }
+
+      await Promise.all([
+        sut.save(saveSurveyResultParams),
+        sut.save(saveSurveyResultParams2),
+        sut.save(saveSurveyResultParams3),
+        sut.save(saveSurveyResultParams4)
+      ])
+
+      const rersult = await sut.loadBySurveyId(survey.id as string, account2.id)
+
+      expect(String(rersult.surveyId)).toBe(saveSurveyResultParams.surveyId)
+      expect(rersult.answers[0].count).toBe(3)
+      expect(rersult.answers[0].percent).toBe(75)
+      expect(rersult.answers[1].count).toBe(1)
+      expect(rersult.answers[1].percent).toBe(25)
     })
   })
 })
