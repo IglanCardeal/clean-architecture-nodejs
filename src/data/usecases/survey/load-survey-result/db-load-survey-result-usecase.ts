81e8aa34032ef2e9f81d0fb78d68bd6665ee8120
IglanCardeal <cmtcardeal@outlook.com>
2024-03-30 13:16:35 -0300

    feat: ensure DbLoadSurveyResultUseCase calls LoadSurveyByIdRepository if LoadSurveyResultRepository returns null

--- src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.ts
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result-usecase.ts
@@ -1,15 +1,25 @@
 import {
   LoadSurveyResultRepository,
   SurveyResultModel,
-  LoadSurveyResultUseCase
+  LoadSurveyResultUseCase,
+  LoadSurveyByIdRepository
 } from './db-load-survey-result-usecase.protocols'
 
 export class DbLoadSurveyResultUseCase implements LoadSurveyResultUseCase {
   constructor(
-    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
   ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel> {
-    return await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
+      surveyId
+    )
+
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.load(surveyId)
+    }
+
+    return {} as any
   }
 }
